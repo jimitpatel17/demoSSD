e0f862f6f5ee8d13308d2fa28250db9da129971a
jimitpatel17 <jimit56785@gmail.com>
2024-02-12 19:14:03 +0530

    Initial Commit

---
+++ demoSSD/demoSSDApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoSSDApp.swift
+//  demoSSD
+//
+//  Created by jimit patel on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct demoSSDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
